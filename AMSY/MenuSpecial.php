75475a895aa0abd6e30b777c6c5a6f10ba3a354c
JosueQsd <38577845+JosueQsd@users.noreply.github.com>
2018-04-30 15:56:28 -0600

    Add files via upload

--- AMSY/MenuSpecial.php
+++ AMSY/MenuSpecial.php
@@ -1,1 +1,25 @@
+<?php
+include('../BDConfig.php');
+$re = $linkBD -> query("SELECT * FROM AMS_Usuarios where SerialNumber != 'z'");?>
 
+<table>
+<tr>
+<th>Serial</th>
+<th>Nombre</th>
+<th>Alias</th>
+</tr>
+<?php
+while ($row = $re -> fetch_assoc()){
+$nom = utf8_encode($row['SerialNumber']);
+$ape = utf8_encode($row['NomUser']);
+$edad = $row['AliUser'];
+?>
+<tr>
+<td><?php echo "$nom";?> </td>
+<td><?php echo "$ape";?> </td>
+<td><?php echo "$edad";?> </td>
+</tr>
+<?php
+}
+?>
+</table>
