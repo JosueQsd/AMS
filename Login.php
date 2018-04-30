86a790987795f29c6de20e97570b57e4044348ca
JosueQsd <38577845+JosueQsd@users.noreply.github.com>
2018-04-30 15:54:12 -0600

    Add files via upload

--- Login.php
+++ Login.php
@@ -5,6 +5,7 @@ $user = $_POST['User'];
 $pass = sha1($_POST['Pass']);
 
 $QcAd = $linkBD -> query("SELECT * FROM AMS_Adminusers where AliUser = '$user' and PassUser = '$pass'");
+$QcSp = $linkBD -> query("SELECT * FROM AMS_SpecialUsers where AliUser = '$user' and PassUser = '$pass'");
 $QcUs = $linkBD -> query("SELECT * FROM AMS_Usuarios where AliUser = '$user' and PassUser = '$pass'");
 
 if ($QcAd->num_rows > 0) {
@@ -13,11 +14,9 @@ if ($QcAd->num_rows > 0) {
    $NUser = utf8_encode($row['NomUser']);
     $_SESSION['loggedin'] = true;
     $_SESSION['username'] = $NUser;
-    $_SESSION['tipUsr'] = true;
-    $_SESSION['intervalo']  = 10;
+    $_SESSION['tipUsr'] = 3;
+    $_SESSION['intervalo']  = 5;
     $_SESSION['inicio'] = time();
-
-
 header("Location: AMSMenu.php");
 }
  } else if ($QcUs->num_rows > 0) {
@@ -27,12 +26,25 @@ header("Location: AMSMenu.php");
      $NUser = utf8_encode($row['NomUser']);
       $_SESSION['loggedin'] = true;
       $_SESSION['username'] = $NUser;
-      $_SESSION['tipUsr'] = false;
-      $_SESSION['intervalo']  = 5;
+      $_SESSION['tipUsr'] = 1;
+      $_SESSION['intervalo']  = 15;
       $_SESSION['inicio'] = time();
 
 header("Location: AMSMenu.php");
  }
+}else if ($QcSp->num_rows > 0) {
+
+  $row = $QcSp->fetch_assoc();
+  if ($Pass = $row['PassUser']) {
+    $NUser = utf8_encode($row['NomUser']);
+     $_SESSION['loggedin'] = true;
+     $_SESSION['username'] = $NUser;
+     $_SESSION['tipUsr'] = 2;
+     $_SESSION['intervalo']  = 10;
+     $_SESSION['inicio'] = time();
+
+header("Location: AMSMenu.php");
+}
 }else{
   echo ("<script LANGUAGE='JavaScript'>
     window.alert('Usuario o Contraseña Incorrectos');
