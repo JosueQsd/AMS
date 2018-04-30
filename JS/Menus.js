3e759ce1e69e383dc216cd340fff67d5f07edb28
JosueQsd <38577845+JosueQsd@users.noreply.github.com>
2018-04-30 15:53:02 -0600

    Update Menus.js

--- JS/Menus.js
+++ JS/Menus.js
@@ -34,7 +34,6 @@ function User (){
   xmlhttp.send();
 
 
-
 var intevalo = setInterval('load_User ()',20000);
 }
 function load_User (){
@@ -46,4 +45,29 @@ document.getElementById("MenuUser").innerHTML=xmlhttp.responseText;
 }
 xmlhttp.open("GET","AMSX/MenuUsuario.php", true);
 xmlhttp.send();
+}
+
+
+function Special (){
+  xmlhttp=new XMLHttpRequest();
+  xmlhttp.onreadystatechange=function() {
+  if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+  document.getElementById("MenuSpecial").innerHTML=xmlhttp.responseText;
+  }
+  }
+  xmlhttp.open("GET","AMSY/MenuSpecial.php", true);
+  xmlhttp.send();
+
+
+var intevalo = setInterval('load_Special ()',20000);
+}
+function load_Special (){
+  xmlhttp=new XMLHttpRequest();
+  xmlhttp.onreadystatechange=function() {
+  if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+  document.getElementById("MenuSpecial").innerHTML=xmlhttp.responseText;
+  }
+  }
+  xmlhttp.open("GET","AMSY/MenuSpecial.php", true);
+  xmlhttp.send();
 }
