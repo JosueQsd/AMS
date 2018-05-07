d48f6ea701f5e8c2efe3964cad220505b02b812e
JosueQsd <38577845+JosueQsd@users.noreply.github.com>
2018-05-07 10:37:05 -0600

    Add files via upload

--- AMSZ/MenuAdmin.php
+++ AMSZ/MenuAdmin.php
@@ -37,27 +37,32 @@ if ($_SESSION['tipUsr'] == 3){
 
   <!-- Custom styles for this template -->
   <link href="../css/simple-sidebar.css" rel="stylesheet">
+
+<style>
+
+.letrasbody{
+  color: #fff;
+}
+
+ul{
+  color : rgba(255, 255, 255, 0.5);
+}
+
+body{
+  background-image: url("http://10.20.60.158:8080/AMS/Resources/Fondo.jpg");
+  background-repeat: no-repeat;
+  background-size: 100%;
+  background-position: center center;
+  background-attachment: fixed;}
+}
+
+</style>
+
+
 </head>
 
 <body>
-
-  <nav class="navbar navbar-expand-md navbar-dark" style= "background-color: #292c80;">
-    <a href="#menu-toggle" class="btn btn-secondary" id="menu-toggle"><i class="material-icons">view_headline</i></a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="collapsibleNavbar">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" href="#">Bienvenido <?php echo "$usuario"; ?></a>
-        </li>
-      </ul>
-      <ul class="navbar-nav flex-row ml-md-auto d-none d-md-flex">
-          <li class="nav-item nav-right mr-md-2"><a class="navbar-brand" href="http://10.20.60.158:8080/AMS/index.php">AMS-SOFTWARE</a></li>
-        </ul>
-    </div>
-  </nav>
-    <div id="wrapper">
+<div id="wrapper" class = "sticky-top">
         <!-- Sidebar -->
         <div id="sidebar-wrapper">
             <ul class="sidebar-nav">
@@ -67,28 +72,76 @@ if ($_SESSION['tipUsr'] == 3){
                     </a>
                 </li>
                 <li>
-                    <a href="#">Roles de Usuarios</a>
+                    <a data-toggle="collapse" href="#usuarios"><i class="material-icons">people</i> Roles de Usuarios</a>
+        <div id="usuarios" class="collapse">
+        <ul>
+          <li><a data-toggle="tab" href="#menu1"><i class="material-icons">supervisor_account</i> Administradores</a></li>
+          <li><a href="#section2"><i class="material-icons">person</i> Especialistas</a></li>
+          <li><a href="#section3"><i class="material-icons">perm_identity</i>Usuarios </a></li>
+        </ul>
+      </div>
                 </li>
                 <li>
-                    <a href="#">Información de Activos</a>
+                    <a href="#"><i class="material-icons">assignment</i> Información de Activos</a>
                 </li>
                 <li>
-                    <a href="#">Financiamiento de Activos</a>
+                    <a href="#"><i class="material-icons">monetization_on</i> Financiamiento</a>
                 </li>
                 <li>
-                    <a href="#">Configuracion de Diseño</a>
+                    <a href="#"><i class="material-icons">style</i> Configuracion de Diseño</a>
                 </li>
                 <li>
-                    <a href="#">Control de Proyectos</a>
+                    <a href="#"><i class="material-icons">domain</i> Control de Proyectos</a>
                 </li>
                 <li>
-                    <a href="http://10.20.60.158:8080/AMS/logout.php">Cerrar Sesión</a>
+                    <a href="http://10.20.60.158:8080/AMS/logout.php"><i class="material-icons">lock</i> Cerrar Sesión</a>
                 </li>
             </ul>
         </div>
-        <img src = "../resources/1.jpg" />
+
+          <nav class="navbar navbar-expand-md navbar-dark sticky-top" style= "background-color: #292c80;">
+            <a href="#menu-toggle" class="btn" style = "color: #fff;" id="menu-toggle"><i class="material-icons">view_headline</i></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="collapsibleNavbar">
+              <ul class="navbar-nav">
+                <li class="nav-item">
+                  <a class="nav-link active" href="#">Bienvenido <?php echo "$usuario"; ?></a>
+                </li>
+              </ul>
+              <ul class="navbar-nav flex-row ml-md-auto d-none d-md-flex">
+                  <li class="nav-item nav-right mr-md-2"><a class="navbar-brand" href="http://10.20.60.158:8080/AMS/index.php">AMS-SOFTWARE</a></li>
+                </ul>
+            </div>
+          </nav>
+
         <div id="page-content-wrapper">
             <div class="container-fluid">
+              <div class="row letrasbody">
+                <div class="col-sm-4">
+                  <h2>About Me</h2>
+                  <h5>Photo of me:</h5>
+                  <div class="fakeimg">Fake Image</div>
+                  <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
+                  <h3>Some Links</h3>
+                  <p>Lorem ipsum dolor sit ame.</p>
+                  <hr class="d-sm-none">
+                </div>
+                <div class="col-sm-8">
+                  <h2>TITLE HEADING</h2>
+                  <h5>Title description, Dec 7, 2017</h5>
+                  <div class="fakeimg">Fake Image</div>
+                  <p>Some text..</p>
+                  <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+                  <br>
+                  <h2>TITLE HEADING</h2>
+                  <h5>Title description, Sep 2, 2017</h5>
+                  <div class="fakeimg">Fake Image</div>
+                  <p>Some text..</p>
+                  <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+                </div>
+              </div>
             </div>
         </div>
         <!-- /#page-content-wrapper -->
